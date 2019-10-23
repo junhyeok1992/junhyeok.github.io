cce906b8c440b9cc996ab846d6eb818ebb8f2282
junhyeok1992 <45192486+junhyeok1992@users.noreply.github.com>
2019-10-24 04:44:39 +0900

    Update simplestorage.sol

--- simplestorage.sol
+++ simplestorage.sol
@@ -1,13 +1,39 @@
 pragma solidity ^0.4.0; 
+
  
-contract SimpleStorage {
-    uint storedData;
- 
-    function set(uint x) {
-        storedData = x;
+
+contract COFFIX {
+
+    uint Data_1;
+    uint Data_2;
+    uint Data_3;
+    uint Data_4;
+    uint Data_5;
+    uint rate;
+    
+    function set_1(uint a) {
+        Data_1 = a;
+    }
+    
+        function set_2(uint b) {
+        Data_2 = b;
+    }
+    
+        function set_3(uint c) {
+        Data_3 = c;
+    }
+    
+        function set_4(uint d) {
+        Data_4 = d;
+    }
+
+    function set_5(uint e) {
+        Data_5 = e;
     }
  
     function get() constant returns (uint) {
-        return storedData;
+        rate = (Data_1+Data_2+Data_3+Data_4+Data_5)/5;
+        return rate;
     }
+
 }
